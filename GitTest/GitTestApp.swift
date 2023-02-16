b9b091deef42e655efaa6066f343dc05e8d07c15
An Vu <antvu22@gmail.com>
2023-02-16 11:31:37 -0500

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by An Vu on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
